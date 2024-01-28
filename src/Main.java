f254d6ba1429dd48f87081888d1bf95f8bded40b
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2024-01-28 19:38:24 +0300

    S15D3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import com.workintech.Employee.Employee;
+
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("****************");
+
+        List <Employee> employees= new LinkedList<>();
+        employees.add(new Employee(1,"Beren","Ateş"));
+        employees.add(new Employee(1,"Beren","Ateş"));
+        employees.add(new Employee(2,"Buse","Kaplan"));
+        employees.add(new Employee(2,"Buse","Kaplan"));
+        employees.add(new Employee(3,"Ali","Yıldırım"));
+        employees.add(new Employee(3,"Ali","Yıldırım"));
+        employees.add(new Employee(4,"Zeynep","Özkan"));
+        employees.add(new Employee(4,"Zeynep","Özkan"));
+        employees.add(new Employee(5,"Sema","Deniz"));
+
+
+        Map<Integer,Employee> employeeMap=new HashMap<>();
+        List<Employee> removeEmployee= new ArrayList<>();
+
+        Iterator<Employee> iterator= employees.iterator();
+        while (iterator.hasNext()){
+            Employee employee=iterator.next();
+            if (employee==null){
+                System.out.println("Null olan bir data bulunmuştur");
+                continue;
+            }
+            if (employeeMap.containsKey(employee.getId())){
+                removeEmployee.add(employee);
+                iterator.remove();
+            } else {
+                employeeMap.put(employee.getId(), employee);
+            }
+
+        }
+        System.out.println("Mevcut olanlar:");
+        System.out.println(employees);
+        System.out.println("Tek olanlar:");
+        System.out.println(employeeMap);
+        System.out.println("Silinenler:");
+        System.out.println(removeEmployee);
+
+
+
     }
 }
---
+++ src/com/workintech/Employee/Employee.java
@@ -0,0 +1,61 @@
+package com.workintech.Employee;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastname;
+
+    public Employee(int id, String firstName, String lastname) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id,employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
